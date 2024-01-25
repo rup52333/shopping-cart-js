256446c03ba44903dd6277c76e6a1f4126781c40
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2024-01-25 20:23:33 +0530

    ne change

--- cart.js
+++ cart.js
@@ -4,7 +4,7 @@ let ShoppingCart = document.getElementById("shopping-cart");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let calculation=()=>{
-    let cartIcon=document.getElementById("cartAmmount");
+    let cartIcon=document.getElementById("cartAmount");
     cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
     }
     calculation();
